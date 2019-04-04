809bff3793bd9b8509aef8841b312e3ed8d5042b
ddot727 <ddot727@gmail.com>
2019-04-04 17:58:27 -0400

    set up routes for users and auth

---
+++ users/users-router.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+const Users = require("../users/users-model.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({
+      message: "error getting users"
+    });
+  }
+});
+
+module.exports = router;
